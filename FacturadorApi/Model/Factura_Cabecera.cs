2a54ff220389a3623057145f4e83dd789ac3adeb
Brian Campagnoni <briancampagnoni96@gmail.com>
2022-09-04 05:59:41 -0300

    Se creo el modelo de Factura_Cabecera

---
+++ FacturadorApi/Model/Factura_Cabecera.cs
@@ -0,0 +1,17 @@
+﻿namespace FacturadorApi.Model
+{
+    public class Factura_Cabecera
+    {
+        public string FC_ID { get; set; }
+        public string Estado { get; set; }
+        public DateOnly FechaAlta { get; set; }
+        public int Cli_ID { get; set; }
+    }
+}
+
+//create table Factura_Cabecera(
+//	FC_ID int Primary Key identity(2000,1) not null,
+//	FechaAlta date,
+//	Estado nvarchar(50),
+//	Cli_ID int Foreign Key References Cliente(Cli_ID) not null
+//)
