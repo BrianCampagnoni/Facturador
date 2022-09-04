615458ad851a88b95b84bb0ed3df518ef691d646
Brian Campagnoni <briancampagnoni96@gmail.com>
2022-09-04 06:38:24 -0300

    Modelos

---
+++ FacturadorApi/Model/Factura_Detalle.cs
@@ -0,0 +1,26 @@
+﻿namespace FacturadorApi.Model
+{
+    public class Factura_Detalle
+    {
+        public int FC_DTL_ID { get; set; }
+        public DateOnly FechaAlta { get; set; }
+        public decimal Cant { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Monto { get; set; }
+        public int Fact_ID { get; set; }
+        public int Art_ID { get; set; }
+
+
+    }
+}
+
+
+//create table Factura_Detalle(
+//	FC_DTL_ID int Primary Key Identity(1,1) not null,
+//	FechaAlta date,
+//	Cant decimal,
+//	Precio decimal,
+//	Monto decimal,
+//	Fact_ID int Foreign Key References Factura_Cabecera(FC_ID) not null,
+//	Art_ID int Foreign Key References Articulo(ART_ID) not null,
+//)
